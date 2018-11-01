255745aa6467821037856ef0b3163436ed7cfde5
Ben Lee <bendelonlee@gmail.com>
2018-11-01 15:52:41 -0600

    Adds vehicle test

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -1,2 +1,11 @@
 require './test/test_helper'
 require './lib/vehicle'
+
+class VehicleTest < Minitest::Test
+  def setup
+    @vehicle = Vehicle.new
+  end
+  def test_it_exists
+    assert_instance_of Vehicle, @vehicle
+  end
+end
